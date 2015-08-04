d1faf3ca68fd23ee3081a0e7bd7c392b8aab3bcb
Richard Feldman <richard.t.feldman@gmail.com>
2015-08-04 11:48:03 -0700

    Use four spaces to indent code sample, not two!

--- src/String/Extra.elm
+++ src/String/Extra.elm
@@ -13,9 +13,10 @@ import String
 followed by a space, followed by either the singular string if the number was 1,
 or the plural string otherwise.
 
-  pluralize "elf" "elves" 2 == "2 elves"
-  pluralize "elf" "elves" 1 == "1 elf"
-  pluralize "elf" "elves" 0 == "0 elves"
+    pluralize "elf" "elves" 2 == "2 elves"
+    pluralize "elf" "elves" 1 == "1 elf"
+    pluralize "elf" "elves" 0 == "0 elves"
+
 -}
 pluralize : String -> String -> number -> String
 pluralize singular plural count =
