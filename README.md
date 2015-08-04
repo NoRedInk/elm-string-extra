d810b909c1730620845fdc657011514d247dbea4
Richard Feldman <rtfeldman@users.noreply.github.com>
2015-08-04 11:29:22 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Convenience functions for working with Strings in Elm
+
+These functions supplement Elm's core [String package](http://package.elm-lang.org/packages/elm-lang/core/latest/String).
+
+---
+[![NoRedInk](https://cloud.githubusercontent.com/assets/1094080/9069346/99522418-3a9d-11e5-8175-1c2bfd7a2ffe.png)][team]
+[team]: http://noredink.com/about/team
+
---
+++ src/String/Extra.elm
@@ -0,0 +1,25 @@
+module String.Extra where
+{-| Convenience functions for working with Strings
+
+# Formatting numbers
+@docs pluralize
+
+-}
+
+import String
+
+
+{-| Given a number, a singular string, and a plural string, returns the number
+followed by a space, followed by either the singular string if the number was 1,
+or the plural string otherwise.
+
+  pluralize "elf" "elves" 2 == "2 elves"
+  pluralize "elf" "elves" 1 == "1 elf"
+  pluralize "elf" "elves" 0 == "0 elves"
+-}
+pluralize : String -> String -> number -> String
+pluralize singular plural count =
+    if count == 1 then
+        "1 " ++ singular
+    else
+        (toString count) ++ " " ++ plural
